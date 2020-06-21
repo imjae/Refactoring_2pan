70fa03f55616d0a42d16d4656bc40c0ab547f86e
jake <jakeim1111@gmail.com>
2020-06-21 17:12:59 +0900

    임시변수 질의함수로변경 변수 인라인하기 함수선언 바꾸기 적용

--- Chapter1/main.js
+++ Chapter1/main.js
@@ -9,45 +9,45 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        
-        let thisAmount = amountFor(perf, play);
-
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
 
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === ${play.name}) volumeCredits += Math.floor(perf.audience / 5);
+        if ("comedy" === ${playFor(perf).name}) volumeCredits += Math.floor(perf.audience / 5);
 
         // 청구내역을 출력한다
-        result += '  ${play.name}: ${format(thisAmount/100)} (${perf.audience}석\n';
-        totalAmount += thisAmount;
+        result += '  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석\n';
+        totalAmount += amountFor(perf);
     }
     result += '총액: ${format(totalAmount/100)}\n';
     result += '적립 포인트: ${volumeCredits}점\n';
     return result;
 }
 
-function amountFor(perf, play) {
-    let thisAmount = 0;
+function amountFor(aPerformance) {
+    let result = 0;
 
-    switch(play.type) {
+    switch(playFor(aPerformance).type) {
         case "tragedy": // 비극
-            thisAmount = 40000;
-            if (perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
-            if (perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error("알 수 없는 장르: ${play.thpe}");
+            throw new Error("알 수 없는 장르: ${playFor(aPerformance).type}");
     }
 
-    return thisAmount;
+    return result;
+}
+
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
 }
