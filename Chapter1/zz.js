5f797f4236d2fbd3f4c903283178971bb7cbf8c4
jake <jakeim1111@gmail.com>
2020-08-21 15:00:36 +0900

    TestCase 작성 및 mainRe 복습 코드 등록

--- Chapter1/zz.js
+++ Chapter1/zz.js
@@ -1,3 +1,40 @@
+var plays = {
+    "hamlet" : {
+        "name": "Hamlet",
+        "type": "tragedy"
+    },
+    "as-like" : {
+        "name": "As You Like It",
+        "type": "comedy"
+    },
+    "othello" : {
+        "name": "Othello",
+        "type": "tragedy"
+    }
+};
+
+var invoices = 
+[
+    {
+        "customer": "BigCo",
+        "performances": [
+            {
+                "playID": "hamlet",
+                "audience": 55
+            },
+            {
+                "playID": "as-like",
+                "audience": 35
+            },
+            {
+                "playID": "othello",
+                "audience": 40
+            }
+        ]
+    }
+];
+
+
 function test() {
     
 }
