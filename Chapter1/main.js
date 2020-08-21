3d7ae193765f80a96d6e004dc3bc2a882a044f3a
jake <jakeim1111@gmail.com>
2020-08-21 16:42:20 +0900

    [복습] amount 합계 로직 함수 추출 버전

--- Chapter1/main.js
+++ Chapter1/main.js
@@ -2,12 +2,6 @@ function statement(invoice, plays) {
     let totalAmount = 0;
     let volumeCredits = 0;
     let result = '청구 내역 (고객명 : ${invoice.customer})\n';
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", 
-        currency: "USD", 
-        minimumFractionDigits: 2
-    }).format;
-
     
     function amountFor(aPerformance) {
         let result = 0;
@@ -46,6 +40,14 @@ function statement(invoice, plays) {
         return result;
     }
 
+    function format(aNumber) {
+        return new Intl.NumberFormat("en-US", {
+            style: "currency", 
+            currency: "USD", 
+            minimumFractionDigits: 2
+        }).format;
+    }
+
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
 
@@ -53,8 +55,8 @@ function statement(invoice, plays) {
         result += '  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석\n';
         totalAmount += amountFor(perf);
     }
+
     result += '총액: ${format(totalAmount/100)}\n';
     result += '적립 포인트: ${volumeCredits}점\n';
     return result;
 }
-
--- Chapter1/mainRe.js
+++ Chapter1/mainRe.js
@@ -37,32 +37,47 @@ var invoices =
 
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
     let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
 
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format;
-
-
     for (let perf of invoice.performances) {
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if("comedy" === playFor(perf).type) {
-            volumeCredits += Math.floor(perf.audience / 5);
-        }
-
         //  청구 내역을 출력한다.
-        result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
-        totalAmount += amountFor(perf);
+        result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
-    result += `총액 : ${format(totalAmount/100)}\n`;
-    result += `적립 포인트: ${volumeCredits}점\n`;
+    result += `총액 : ${usd(totalAmount(invoice))}\n`;
+    result += `적립 포인트: ${totalVolumeCredits(invoice)}점\n`;
     
+    return result;
+}
+
+function totalAmount(invoice) {        
+    let result = 0;
+    for (let perf of invoice.performances) {
+        result += amountFor(perf);
+    }
+    return result;
+}
+
+function totalVolumeCredits(invoice) {
+    let result = 0;
+    for (let perf of invoice.performances) {
+        result += voumeCreditsFor(perf);
+    }
+    return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format(aNumber/100);
+}
+
+function voumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if("comedy" === playFor(aPerformance).type) {
+        result += Math.floor(aPerformance.audience / 5);
+    }
     return result;
 }
 
