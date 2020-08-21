a705f8b142c13ea82375b1c3f29dd9c9416e7a45
jake <jakeim1111@gmail.com>
2020-08-21 15:35:22 +0900

    [복습] 함수 인라인하기 사용하여 지역변수 제거(play, thisAmount)

--- Chapter1/mainRe.js
+++ Chapter1/mainRe.js
@@ -47,20 +47,17 @@ function statement(invoice, plays) {
 
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = amountFor(perf, play);
-        
         // 포인트를 적립한다.
         volumeCredits += Math.max(perf.audience - 30, 0);
 
         // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if("comedy" === play.type) {
+        if("comedy" === playFor(perf).type) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
 
         //  청구 내역을 출력한다.
-        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
 
     result += `총액 : ${format(totalAmount/100)}\n`;
@@ -69,27 +66,35 @@ function statement(invoice, plays) {
     return result;
 }
 
-function amountFor(perf, play) {
-    let thisAmount = 0;
-    switch(play.type) {
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+
+function amountFor(aPerformance) {
+    //  aPerformance 변수의 경우 함수를 사용하는 loof 변수에서 오기때문에 반복문을 한번 돌때마다 자연스럽게 값이 변경된다.
+    //  하지만 play 는 aPerformance 에서 얻기 때문에 애초에 매개변수로 전달할 필요가 없다.
+    //  amoutFor 함수에서 다시 계산하면 된다.
+    let result = 0;
+    switch(playFor(aPerformance).type) {
         case "tragedy": // 비극
-            thisAmount = 40000;
-            if(perf.audience > 30) {
-                thisAmount += 1000 * (perf.audience - 30);
+            result = 40000;
+            if(aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":
-            thisAmount = 30000;
-            if(perf.audience > 20) {
-                thisAmount += 10000 + 500 * (perf.audience - 20);
+            result = 30000;
+            if(aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            thisAmount += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
-            throw new Error(`알 수 없는 장르 : ${play.type}`);
+            throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
     }
 
-    return thisAmount;
+    return result;
 }
 
 alert(statement(invoices[0], plays));
