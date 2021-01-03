6235b50d6980db17832aa86f5494b2bbde3b3c2e
imjae <code@kakao.com>
2021-01-03 16:00:51 +0900

    switch code extraction to function amountFor()

--- Chapter1_2/statement.js
+++ Chapter1_2/statement.js
@@ -1,78 +1,90 @@
 let plays = {
-    "hamlet": {"name": "Hamlet", "type": "tragedy"},
-    "as-like": {"name": "As You Like It", "type": "comedy"},
-    "othello": {"name": "Othello", "type": "tragedy"}
+  hamlet: { name: "Hamlet", type: "tragedy" },
+  "as-like": { name: "As You Like It", type: "comedy" },
+  othello: { name: "Othello", type: "tragedy" },
 };
 
 let invoices = [
-    {
-        "customer": "BigCo",
-        performances: [
-            {
-                "playID": "hamlet",
-                "audience": 55
-            },
-            {
-                "playID": "as-like",
-                "audience": 35
-            },
-            {
-                "playID": "othello",
-                "audience": 40
-            }
-        ]   
-    }
+  {
+    customer: "BigCo",
+    performances: [
+      {
+        playID: "hamlet",
+        audience: 55,
+      },
+      {
+        playID: "as-like",
+        audience: 35,
+      },
+      {
+        playID: "othello",
+        audience: 40,
+      },
+    ],
+  },
 ];
 
 function statement(invoices, plays) {
-    let totalAmount = 0;
-    let volumeCredits = 0;
-    let result = `청구 내역 (고객명: ${invoices.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency",
-        currency: "USD",
-        minimumFractionDigits: 2
-    }).format;
+  let totalAmount = 0;
+  let volumeCredits = 0;
+  let result = `청구 내역 (고객명: ${invoices.customer})\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format;
 
-    for (let perf of invoices.performances) {
-        const play = plays[perf.playID];
-        let thisAmount = 0;
-
-        switch(play.type) {
-            case "tragedy": { 
-                // 비극
-                thisAmount = 40000;
-                if(perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            }
-            case "comedy": {
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            }
-            default:
-                throw new Error(`알 수 없는 장르: ${play.type}`);
-            
+  // 별도 함수로 빼냈을 때 유효범위를 벗어나는 변수, 즉 새 함수에서는 곧바로 사용할 수 없는 변수가 있는지 확인한다.
+  // 이번 예에서는 perf, play, thisAmount가 여기 속한다.
+  // perf와 play는 추출한 새 함수에서도 필요하지만 값을 변경하지 않기 때문에 매개변수로 전달하면 된다.
+  // 한편 thisAmount는 함수 안에서 값이 바뀌는데, 이런 변수는 조심해서 다뤄야 한다.
+  // 이번 예에서는 이런 변수가 하나뿐이므로 이 값을 반환하도록 작성했다.
+  function amountFor(perf, play) {
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy": {
+        // 비극
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
         }
-        // 포인트를 적립한다.
-        volumeCredits += Math.max(perf.audience - 30, 0);
-        
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
-
-        // 청구 내역을 출력한다.
-        result += `    ${play.name}: ${format(thisAmount/100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        break;
+      }
+      case "comedy": {
+        // 희극
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      }
+      default:
+        throw new Error(`알 수 없는 장르: ${play.type}`);
     }
+    return thisAmount;
+  }
 
-    result += `총액: ${format(totalAmount/100)}\n`;
-    result += `적립 포인트 : ${volumeCredits}점\n`;
-    return result;
+  for (let perf of invoices.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = amountFor(perf, play);
+
+    // 포인트를 적립한다.
+    volumeCredits += Math.max(perf.audience - 30, 0);
+
+    // 희극 관객 5명마다 추가 포인트를 제공한다.
+    if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+    // 청구 내역을 출력한다.
+    result += `    ${play.name}: ${format(thisAmount / 100)} (${
+      perf.audience
+    }석)\n`;
+    totalAmount += thisAmount;
+  }
+
+  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `적립 포인트 : ${volumeCredits}점\n`;
+  return result;
 }
 
 console.log(statement(invoices[0], plays));
