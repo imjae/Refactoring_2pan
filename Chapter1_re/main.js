f7fec39057e0074a3ff78edfcea5dfb606a8f548
imjae <code@kakao.com>
2021-02-04 18:58:50 +0900

    play와같은 임시변수 질의함수로 변경

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -37,38 +37,43 @@ const statement = (invoice, plays) => {
   let totalAmout = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
+
   const format = new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     minimumFractionDigits: 2,
   }).format;
 
-  const amountFor = (perf, play) => {
-    let thisAmount = 0;
+  const amountFor = (aPerformance, play) => {
+    let result = 0;
 
     switch (play.type) {
       case "tragedy": // 비극
-        thisAmount = 40000;
-        if (perf.audience > 30) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 40000;
+        if (aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
         }
         break;
       case "comedy": // 희극
-        thisAmount = 30000;
-        if (perf.audience > 20) {
-          thisAmount += 1000 * (perf.audience - 30);
+        result = 30000;
+        if (aPerformance.audience > 20) {
+          result += 1000 * (aPerformance.audience - 30);
         }
-        thisAmount += 300 * perf.audience;
+        result += 300 * aPerformance.audience;
         break;
       default:
         throw new Error(`알 수 없는 장르 : ${play.type}`);
     }
 
-    return thisAmount;
+    return result;
+  };
+
+  const playFor = (aPerformance) => {
+    return plays[aPerformance.playID];
   };
 
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // 포인트를 적립한다.
