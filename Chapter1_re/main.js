354f29f07156d13a62f27c3051f511bb13c6284c
imjae <code@kakao.com>
2021-02-04 19:01:28 +0900

    변수 인라인후 해당변수가 사용하던 매개변수 삭제

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -44,10 +44,10 @@ const statement = (invoice, plays) => {
     minimumFractionDigits: 2,
   }).format;
 
-  const amountFor = (aPerformance, play) => {
+  const amountFor = (aPerformance) => {
     let result = 0;
 
-    switch (play.type) {
+    switch (playFor(aPerformance).type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -62,7 +62,7 @@ const statement = (invoice, plays) => {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르 : ${play.type}`);
+        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
     }
 
     return result;
@@ -73,7 +73,7 @@ const statement = (invoice, plays) => {
   };
 
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    let thisAmount = amountFor(perf);
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
