fe6b7015f2cdf1e15ab3f5a7ca860327c493d28e
imjae <code@kakao.com>
2021-02-04 20:08:51 +0900

    renderPlainText함수에서 인자로 받던 invoice를 제거

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -33,13 +33,16 @@ const invoices = [
   },
 ];
 
-function statement (invoice, plays) {
-  return renderPlainText(invoice, plays);
+function statement(invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances;
+  return renderPlainText(statementData, plays);
 };
 
-function renderPlainText(invoice, plays) {
-  let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
-  for (let perf of invoice.performances) {
+function renderPlainText(data, plays) {
+  let result = `청구 내역 (고객명 : ${data.customer})\n`;
+  for (let perf of data.performances) {
     result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   result += `총액: ${usd(totalAmount())}\n`;
@@ -95,7 +98,7 @@ function renderPlainText(invoice, plays) {
 
   function totalAmount ()  {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       // 청구 내역을 출력한다.
       result += amountFor(perf);
     }
@@ -104,7 +107,7 @@ function renderPlainText(invoice, plays) {
   
   function totalVolumeCredits ()  {
     let result = 0;
-    for (let perf of invoice.performances) {
+    for (let perf of data.performances) {
       result += volumeCreditsFor(perf);
     }
     return result;
