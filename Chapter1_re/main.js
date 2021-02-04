cbaea9718be128ba0c0e5c765d415533c31d651e
imjae <code@kakao.com>
2021-02-04 20:24:35 +0900

    invoice.performances 배열을 얕은복사로 변경 후 renderPlainText의 인자로 전달한다.

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -36,14 +36,24 @@ const invoices = [
 function statement(invoice, plays) {
   const statementData = {};
   statementData.customer = invoice.customer;
-  statementData.performances = invoice.performances;
+  statementData.performances = invoice.performances.map(enrichPerformance);
   return renderPlainText(statementData, plays);
+
+  function enrichPerformance(aPerformance) {
+    const result = Object.assign({}, aPerformance);
+    result.play = playFor(result);
+    return result;
+  }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  };
 };
 
 function renderPlainText(data, plays) {
   let result = `청구 내역 (고객명 : ${data.customer})\n`;
   for (let perf of data.performances) {
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+    result += `${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
   }
   result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
@@ -60,7 +70,7 @@ function renderPlainText(data, plays) {
   function amountFor(aPerformance) {
     let result = 0;
 
-    switch (playFor(aPerformance).type) {
+    switch (aPerformance.play.type) {
       case "tragedy": // 비극
         result = 40000;
         if (aPerformance.audience > 30) {
@@ -75,15 +85,13 @@ function renderPlainText(data, plays) {
         result += 300 * aPerformance.audience;
         break;
       default:
-        throw new Error(`알 수 없는 장르 : ${playFor(aPerformance).type}`);
+        throw new Error(`알 수 없는 장르 : ${aPerformance.play.type}`);
     }
 
     return result;
   };
 
-  function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-  };
+  
 
   function volumeCreditsFor(aPerformance) {
     // 포인트를 적립한다.
@@ -91,7 +99,7 @@ function renderPlainText(data, plays) {
     result += Math.max(aPerformance.audience - 30, 0);
 
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if("comedy" === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    if("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
 
     return result;
   };
