3f80458e22cfcd71c01bd4ae861a60d2d89ca95b
imjae <code@kakao.com>
2021-02-04 19:40:29 +0900

    totalAmount 함수 쪼갠후 appleSauce라는 임의의 함수명 준다

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -34,7 +34,6 @@ const invoices = [
 ];
 
 const statement = (invoice, plays) => {
-  let totalAmout = 0;
   let result = `청구 내역 (고객명 : ${invoice.customer})\n`;
 
   const usd = (aNumber) => {
@@ -83,12 +82,16 @@ const statement = (invoice, plays) => {
 
     return result;
   };
-
-  for (let perf of invoice.performances) {
-    // 청구 내역을 출력한다.
-    result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
-    totalAmout += amountFor(perf);
-  }
+  const appleSauce = () => {
+    let totalAmount = 0;
+    for (let perf of invoice.performances) {
+      // 청구 내역을 출력한다.
+      result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+      totalAmount += amountFor(perf);
+    }
+  };
+  let totalAmount = appleSauce();
+  
   
   const totalVolumeCredits = () => {
     let volumeCredits = 0;
@@ -98,7 +101,7 @@ const statement = (invoice, plays) => {
     return volumeCredits;
   };
 
-  result += `총액: ${usd(totalAmout)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 };
