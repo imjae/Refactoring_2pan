d5aed76e723003f6cdbab8b6deb153af83ba3ced
imjae <code@kakao.com>
2021-02-04 19:42:58 +0900

    임시변수와 추출할 함수의 이름이 겹칠때는 임의의 이름을 주고 추출한수 인라인시킨후 변경한다

--- Chapter1_re/main.js
+++ Chapter1_re/main.js
@@ -82,16 +82,15 @@ const statement = (invoice, plays) => {
 
     return result;
   };
-  const appleSauce = () => {
+  const totalAmount = () => {
     let totalAmount = 0;
     for (let perf of invoice.performances) {
       // 청구 내역을 출력한다.
       result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
       totalAmount += amountFor(perf);
     }
+    return totalAmount;
   };
-  let totalAmount = appleSauce();
-  
   
   const totalVolumeCredits = () => {
     let volumeCredits = 0;
@@ -101,7 +100,7 @@ const statement = (invoice, plays) => {
     return volumeCredits;
   };
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumeCredits()}점\n`;
   return result;
 };
